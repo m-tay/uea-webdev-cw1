2fd7169f6dd1172b61f3def133719f966c84a789
Matthew Taylor <ecstatic@gmail.com>
2017-12-01 21:06:15 +0000

    Updated reviews, merging with Jez's code

--- hotel.py
+++ hotel.py
@@ -4,27 +4,33 @@ import csv
 import datetime
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/booking')
+def booking():
+    
+    bookings = readfile('static\\bookings.csv')
+
+    return render_template('booking.html', bookinglist = bookings)
+
+    
+@app.route('/reviews', methods = ['GET'])
+def reviews():
+
+    reviews = readfile('static\\reviews.csv')
+
+    return render_template('reviews.html', reviewlist = reviews)
 
 @app.route('/gallery')
 def gallery():
     return render_template('gallery.html')
 
-@app.route('/reviews', methods = ['GET'])
-def reviews():
-
-    currentreviews = readfile('static\\reviews.csv')
-
-    return render_template('reviews.html', reviewlist=currentreviews)
-
-
 @app.route('/guestReview', methods=['POST'])
 def guestreview():
     # Read in existing rows
-    currentreviews = readfile('static\\reviews.csv')
+    reviews = readfile('static\\reviews.csv')
 
     # Create and add new review
     date = datetime.datetime.now().date().strftime("%x")
@@ -33,14 +39,45 @@ def guestreview():
     comments = request.form[('comments')]
 
     newRow = [date, name, rating, comments]
-    currentreviews.append(newRow)
+    reviews.append(newRow)
 
     # Save new review list back to file
-    writefile('static\\reviews.csv', currentreviews)
+    writefile('static\\reviews.csv', reviews)
 
     # Reload page
-    currentreviews = readfile('static\\reviews.csv')
-    return render_template('reviews.html', reviewlist=currentreviews)
+    reviews = readfile('static\\reviews.csv')
+    return render_template('reviews.html', reviewlist = reviews)
+
+@app.route('/guestBooking', methods=['POST'])
+def guestbooking():
+    # Read in existing rows
+    bookings = readfile('static\\bookings.csv')
+
+    # Create and add new review
+    date = datetime.datetime.now().date().strftime("%x")
+    name = request.form[('name')]
+    email = request.form[('email')]
+    arrival = request.form[('arrival')]
+    departure = request.form[('departure')]
+    confirmed = "no"
+
+    newRow = [date, name, email, arrival, departure]
+    bookings.append(newRow)
+
+    # Save new booking list back to file
+    writefile('static\\bookings.csv', bookings)
+
+    # Reload page
+    #print(bookings[0])
+    #for row in bookings:
+    #    print(row)
+    #    for index in row:
+    #        print(row[index])
+	#
+    row = 0
+    index = 1
+    print(bookings[row[index]])
+    return render_template('booking.html', bookinglist = bookings)
 
 
 def readfile(filename):
@@ -55,6 +92,7 @@ def writefile(filename, list):
         writer = csv.writer(outFile)
         writer.writerows(list)
     return
+
 
 
 if __name__ == '__main__':
